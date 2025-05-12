4f20330bb128d62661eb0197325520a41f30dd57
Shirin <esshirin486@gmail.com>
2025-05-13 00:53:32 +0200

    vite

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -6,4 +6,8 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   base: "/", // ✅ important for correct path resolution
   plugins: [react(), tailwindcss()],
+  server: {
+    host: "0.0.0.0", // Expose the server to all network interfaces
+    port: 8080, // Set a specific port for deployment (Render usually expects 8080)
+  },
 });
