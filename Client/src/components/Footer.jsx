20742df2ecee7d29a092a32df944730a72373e77
Shirin <esshirin486@gmail.com>
2025-05-15 12:54:52 +0200

    tailwind

--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -3,24 +3,26 @@ import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="bg-white border-t text-sm py-6 px-4">
-      <div className="max-w-6xl mx-auto flex flex-col sm:flex-row justify-between items-center gap-2">
+    <footer className="bg-white border-t border-gray-200 text-sm py-6 px-4 font-kodchasan">
+      <div className="max-w-6xl mx-auto flex flex-col sm:flex-row sm:justify-between items-center gap-4">
+        {/* Links */}
         <div className="flex flex-col sm:flex-row items-center gap-4">
           <Link
             to="/cookie-consent"
-            className="hover:underline transition text-[#037c92] font-kodchasan hover:text-[#f7941c]"
+            className="hover:underline text-[#037c92] hover:text-[#f7941c] transition"
           >
             Cookie Policy
           </Link>
           <Link
             to="/privacy-policy"
-            className="hover:underline text-[#037c92] font-kodchasan hover:text-[#f7941c] transition"
+            className="hover:underline text-[#037c92] hover:text-[#f7941c] transition"
           >
             Privacy Policy
           </Link>
         </div>
 
-        <div className="flex gap-4 items-center mt-4 sm:mt-0">
+        {/* Social Icons */}
+        <div className="flex gap-4 items-center">
           <a
             href="https://www.linkedin.com/in/shirinhajiesmaeili/"
             target="_blank"
@@ -39,7 +41,8 @@ const Footer = () => {
           </a>
         </div>
 
-        <p className="text-xs text-gray-500 mt-4 sm:mt-0">
+        {/* Copyright */}
+        <p className="text-xs text-gray-500 text-center sm:text-right">
           &copy; {new Date().getFullYear()} iTiN. All rights reserved.
         </p>
       </div>
